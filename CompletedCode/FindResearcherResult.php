4ae1340178867b8e75eecbf90ffc8ee64f761a93
ReedRodgers <reed.rodgers@gmail.com>
2017-07-19 00:35:55 -0400

    Add files via upload

--- CompletedCode/FindResearcherResult.php
+++ CompletedCode/FindResearcherResult.php
@@ -7,7 +7,7 @@
 <ul id="tabs">
     <li><a href="Home.html">Home</a></li>
     <li><a href="Admin.html">Admin</a></li>
-    <li.selected><a href="Find%20Researcher.html">Find Researcher</a></li.selected>
+    <li class="selected"><a href="Find%20Researcher.html">Find Researcher</a></li>
     <li><a href="Find%20Institution.html">Find Institution</a></li>
     <li><a href="Find%20Object.html">Find Object</a></li>
     <li><a href="Locate%20Planet.html">Locate Planet</a></li>
@@ -26,17 +26,37 @@ $mysqli = get_mysqli_conn();
 
 <?php
 
-$sql = 'Select r.first_name, r.last_name, r.income 
-From Researchers as r, Projects as p, Projects_rid as pr 
-Where p.oid = ? and r.rid = pr.rid and pr.pname = p.pname';
+$sql = 'SELECT DISTINCT r.first_name, r.last_name, r.income 
+FROM Researchers as r, Projects as p, Projects_rid as pr 
+WHERE r.rid = pr.rid AND pr.pname = p.pname 
+AND pr.rid IN 
+    (SELECT pr1.rid 
+     FROM Projects_rid AS pr1, Projects AS p1, Deep_Space_Object 
+     INNER JOIN Objects2 ON Deep_Space_Object.oid = Objects2.oid 
+     WHERE Deep_Space_Object.oid = p1.oid 
+     AND pr1.pname = p1.pname 
+    AND Deep_Space_Object.oid NOT IN 
+     (SELECT o2.oid FROM Deep_Space_Object as o2 WHERE o2.type like ?)) 
+AND pr.rid NOT IN 
+    (SELECT pr1.rid 
+     FROM Projects_rid AS pr1, Projects AS p1, Star_System 
+     INNER JOIN Objects2 ON Star_System.oid = Objects2.oid 
+     WHERE Star_System.oid = p1.oid 
+     AND pr1.pname = p1.pname)
+AND pr.rid NOT IN 
+    (SELECT pr1.rid 
+     FROM Projects_rid AS pr1, Projects AS p1, local_system
+     INNER JOIN Objects2 ON local_system.oid = Objects2.oid 
+     WHERE local_system.oid = p1.oid 
+     AND pr1.pname = p1.pname)';
 // Prepared statement, stage 1: prepare
 $stmt = $mysqli-> prepare($sql);
 
 // Prepared statement, stage 2: bind and execute
-$object = $_GET['Object'];
+$type = $_GET['Type']; //object exists
 
 // "i" for integer, "d" for double, "s" for string, "b" for blob
-$stmt-> bind_param('s', $object);
+$stmt-> bind_param('i', $type); //looks good
 
 $stmt-> execute();
 // Bind result variables
